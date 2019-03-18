648b120f1ef2ed8da2325d98bf10b84c1a992261
Kazuhiro Kobayashi <orihuzakihsayabok@gmail.com>
2019-03-18 18:50:20 +0900

    Add infinite spp/time limit

--- src/main.rs
+++ src/main.rs
@@ -35,16 +35,28 @@ fn main() -> Result<(), std::io::Error> {
     std::fs::create_dir_all(&outdir)?;
     let time_limit = matches
         .opt_str("t")
-        .map(|s| s.parse().expect(&format!("failed to prase time {}", s)))
-        .unwrap_or(1.0);
+        .map(|s| {
+            if s == "inf" {
+                None
+            } else {
+                Some(s.parse().expect(&format!("failed to prase time {}", s)))
+            }
+        })
+        .unwrap_or(Some(1.0));
     let report_freq: f64 = matches
         .opt_str("r")
         .map(|s| s.parse().expect(&format!("failed to prase time {}", s)))
         .unwrap_or(5.0);
-    let max_spp: usize = matches
+    let max_spp: Option<usize> = matches
         .opt_str("s")
-        .map(|s| s.parse().expect(&format!("failed to prase time {}", s)))
-        .unwrap_or(1);
+        .map(|s| {
+            if s == "inf" {
+                None
+            } else {
+                Some(s.parse().expect(&format!("failed to prase time {}", s)))
+            }
+        })
+        .unwrap_or(Some(10));
 
     let v = vec![RGB::all(0.0); 10];
     let film = {
@@ -83,32 +95,51 @@ fn main() -> Result<(), std::io::Error> {
                 let elapsed = std::time::Instant::now().duration_since(start);
                 let ms = { elapsed.as_secs() * 1000 + elapsed.subsec_millis() as u64 };
                 let secs = (ms as f64) / 1000.0;
-                let progress = completed_samples as f64 / max_spp as f64;
-                let eta = secs * (1.0 - progress) / progress;
+                let progress = max_spp.map(|max_spp| completed_samples as f64 / max_spp as f64);
+                let eta = progress.map(|progress| secs * (1.0 - progress) / progress);
                 let spd = completed_samples as f64 / secs;
                 let spd_pc = spd / render_config.nthread as f64;
                 info!(
-                    "{} / {} ({:.2} %) elapsed {:.2} sec",
+                    "{} / {} ({} %) elapsed {:.2} sec",
                     completed_samples,
-                    max_spp,
-                    progress * 100.0,
+                    max_spp
+                        .as_ref()
+                        .map(ToString::to_string)
+                        .unwrap_or("Inf".into()),
+                    progress
+                        .map(|x| format!("{:.2}", x * 100.0))
+                        .unwrap_or("N/A".into()),
                     secs,
                 );
-                info!("    ETA {:.2} sec ({:?} for limit)", eta, time_limit - secs);
+                info!(
+                    "    ETA {} sec ({:?} for limit)",
+                    eta.map(|x| format!("{:.2}", x)).unwrap_or("N/A".into()),
+                    time_limit.map(|x| x - secs)
+                );
 
                 info!("    Speed {:.2} spp/sec {:.2} spp/sec/core", spd, spd_pc);
                 //let film = film.lock().unwrap();
                 //film.to_image(RGBPixel::average).write_exr(&format!("output/{}.exr", cycle));
-                if completed_samples >= max_spp {
+                if max_spp
+                    .map(|max_spp| completed_samples >= max_spp)
+                    .unwrap_or(false)
+                {
                     None
-                } else if secs >= time_limit {
+                } else if time_limit.map(|lim| secs >= lim).unwrap_or(false) {
                     info!("stopping due to time limit");
                     None
                 } else {
-                    let rest = max_spp - completed_samples;
-                    let next_cycle_time = report_freq.min(time_limit - secs);
+                    let mut next_cycle_time = report_freq;
+                    if let Some(time_limit) = time_limit {
+                        next_cycle_time = next_cycle_time.min(time_limit - secs);
+                    }
                     let next_report: usize = (next_cycle_time * spd) as usize;
-                    Some(rest.min(next_report).max(1))
+                    if let Some(max_spp) = max_spp {
+                        let rest = max_spp - completed_samples;
+                        Some(rest.min(next_report).max(1))
+                    } else {
+                        Some(next_report.max(1))
+                    }
                 }
             }
         })
